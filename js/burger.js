2ed6f1f9bf6702d05c193ec55c7341d64f405eb3
Kamal Sagderov <sagderovkamal.it@gmail.com>
2025-04-22 01:21:30 +0300

    add chanhes

--- js/burger.js
+++ js/burger.js
@@ -1,28 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
   const burger = document.querySelector('.js-burger');
   const burgerBtn = document.querySelector('.js-burger__btn');
-  const closeBurgerBtn = document.querySelector('.js-close-burger'); // Кнопка закрытия
+  const closeBurgerBtn = document.querySelector('.js-close-burger');
   const body = document.body;
 
   const BURGER_OPEN = 'burger__open';
   const BURGER_BTN_ACTIVE = 'burger-btn__open';
   const BODY_FIXED = 'body_fixed';
 
-  // Переключение по кнопке бургер-меню
   burgerBtn.addEventListener('click', () => {
     burger.classList.toggle(BURGER_OPEN);
     burgerBtn.classList.toggle(BURGER_BTN_ACTIVE);
     body.classList.toggle(BODY_FIXED);
   });
 
-  // Закрытие по клику на кнопку закрытия
   closeBurgerBtn.addEventListener('click', () => {
     burger.classList.remove(BURGER_OPEN);
     burgerBtn.classList.remove(BURGER_BTN_ACTIVE);
     body.classList.remove(BODY_FIXED);
   });
 
-  // Закрытие по клику вне меню и кнопки
   document.addEventListener('click', (event) => {
     const clickInside = event.composedPath().includes(burger) || event.composedPath().includes(burgerBtn);
     if (!clickInside) {
@@ -30,5 +27,15 @@ document.addEventListener('DOMContentLoaded', () => {
       burgerBtn.classList.remove(BURGER_BTN_ACTIVE);
       body.classList.remove(BODY_FIXED);
     }
+  });
+
+  // 👇 Закрытие бургера при клике по ссылке
+  const burgerLinks = burger.querySelectorAll('a');
+  burgerLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      burger.classList.remove(BURGER_OPEN);
+      burgerBtn.classList.remove(BURGER_BTN_ACTIVE);
+      body.classList.remove(BODY_FIXED);
+    });
   });
 });
